95921aa92a41b6fdd03ff800c7db5617eeac1490
ojisamacharles <115052569+ojisamacharles@users.noreply.github.com>
2022-10-28 08:20:21 -0700

    parking_garage.py
    INITIAL PSEUDO-CODE

---
+++ parking_garage.py
@@ -0,0 +1,56 @@
+#3 floors/stories
+#ticketing machingat the beginning, prob like $10, system in the works
+#64 parking spaces available per floor, 8x8, 
+
+if module == 0: #if parking lot is full. 56/64 spots available
+    print("Parking Lot is full.") 
+    break
+
+#valet has a higher rate
+
+if valet.spo full: # *8* valet spots 8/64 dedicated to valet service.
+    print("All parking spaces have been taken, \n> all parking spots have been taken")
+
+from IPython.display import clear_output
+
+class Garage():
+    def __init__(self):
+        variable = {}
+        self.spots = 56
+        self.valetspots = 8
+    
+    def takeTicket()= 
+    updateparkingspaces = 
+    
+    def addcar():
+    def valet.service()=#this contains a lotta sawse. 
+    #Optional car washing service for extra moolah
+        print("")
+    if self.spots > 0:
+        print(f"There are still {self.spots} spots available")
+        print(input("Which service would you like? (basic/ Valet/ Premium Valet:"))
+    elif self.spots = 0:
+        print(f"There are no more spots available. Sorry!")
+        print()
+
+   
+    def spots():
+        spots.available = 
+        self.spots = # num of vacant spots, return spots
+    
+    
+    
+    
+    def payticket()=
+        if paid:
+        print("Thank you, have a nice day")
+        print("Please remember to leave in 15 mins")
+        if not paid:
+            you_pay = (f"Don't forget to pay!")
+            print(input("Did you pay? (y/n): "))
+        
+
+    def leaveGarage()=
+
+    def 
+
