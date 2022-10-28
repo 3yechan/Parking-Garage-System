3d9d7522e6065c131005fd7f0eec1f7ee0697e86
ojisamacharles <115052569+ojisamacharles@users.noreply.github.com>
2022-10-28 12:20:49 -0700

    Add files via upload
    2nd Push, Friday Afternoon. Talked to Alex, Admin Function (generate total revenue) Control Flow, reduce number of methods (maybe 2 or 3), penalty for not paying, work on making the dictionary

--- parking_garage.py
+++ parking_garage.py
@@ -1,14 +1,14 @@
-#3 floors/stories
+#3 floors/stories REJECTED
 #ticketing machingat the beginning, prob like $10, system in the works
 #64 parking spaces available per floor, 8x8, 
 
-if module == 0: #if parking lot is full. 56/64 spots available
-    print("Parking Lot is full.") 
-    break
+# if module == 0: #if parking lot is full. 10/10 spots available
+#     print("Parking Lot is full.") 
+#     break
 
 #valet has a higher rate
 
-if valet.spo full: # *8* valet spots 8/64 dedicated to valet service.
+if valet.spo* full: # *8* valet spots 8/64 dedicated to valet service.
     print("All parking spaces have been taken, \n> all parking spots have been taken")
 
 from IPython.display import clear_output
@@ -21,22 +21,89 @@ from IPython.display import clear_output
 
 
 
+
 class Garage():
     def __init__(self):
-        variable = {}
-        self.spots = 10
-        self.valetspots = 8
+        self.variable = {}
+        self.spots = 10 #b8
+        self.valetspots = 8 #v8
+
+        self.basic = #True or false
+        self.premium = #True or false
+        self.valet = #True or false
+    #
+#for ticket, dict in variable.item(10):
     
-    def takeTicket(): 
-        clear()
-    updateparkingspaces = 
+    #admin functions
+    money = []
+    money_for_the_day = sum(money)
+
+
+
+
+    def Dict():
+    basic
+
+    B8= dict()
+    B8[basic] = 10
+    B8[valet] = 12
+    B8[premium] = 15
+
     
-    def addcar():
+
+    self.variable = ()
+    self.spots = ()
+    self.valetspots = ()
+
+
+ customer1(self, basic, premium, valet)
+    if 
+    def Addcar(): 
         clear()
-        # use to ensure we do not over sell
-        #     def remove(self):
-        # clear()
-        # discard = input("What would you like to discard?")
+    
+        if self.spots > 0:
+            print(f"There are still {self.spots} spots available")
+            response = (input("Which service would you like? (basic/ valet)")
+        if response.lower() == "basic" and self.spots >= 1 and <=10:
+            self.spots -= 1
+            variable = response
+            self.valet()
+        # if response.lower() == "valet":
+        #     self.valet()
+        elif self.spots == 0:
+            print(f"There are no more spots available. Sorry!")
+#  - - - - - - - - - - - - - - - VALET SERVICE ( part of ADDCAR(), still in the works) - - - - - 
+            def valet():
+            if self.valetspots >= 0:
+            print(f"There are still {self.valetspots} spots available for valet service")
+            response = (input("Which service would you like? (Valet/ Premium Valet:"))
+            if response.lower() == "valet" and self.valetspots >= 1 and <=8:
+                self.valetspots -= 1
+                variable = response
+            elif response.lower() == "premium valet" and self.valetspots >= 1 and <=8:
+                self.valetspots -= 1
+                variable = response
+
+#  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+      
+
+    
+        
+
+#-------------------------------------------SCRATCH CODE ------------------------------------------------
+  #updateparkingspaces = 
+    #def addcar():
+    #    clear()  #use to ensure we do not over sell
+            #def remove(self):
+        #clear()
+    # def valet.service():#this contains a lotta sawse. 
+    # #Optional car washing service for extra moolah
+    #     clear()
+    #     print("")
+#----------------------------------------Reference Code from Shopping List Example------------------------
+ 
+        #     discard = input("What would you like to discard?")
         # quantity = int(input("How many would you like to remove?"))
         # try:
         #     self.items[discard] -= quantity
@@ -45,49 +112,96 @@ class Garage():
         #     print(f'{quantity} {discard}(s) have been removed.')
         # except:
         #     print(f'{discard} was not in your cart!')
-        # self.show()
+        # self.show()    
 
-    def valet.service():#this contains a lotta sawse. 
-    #Optional car washing service for extra moolah
+
+    def LeaveGarage():
         clear()
-        print("")
-    if self.spots > 0:
-        print(f"There are still {self.spots} spots available")
-        response = (input("Which service would you like? (basic/ Valet/ Premium Valet:"))
-        if response.lower() == "basic" and self.spots >= 1:
-            self.spots -= 1
-
-        elif response.lower() == "valet" and self.valetspots >= 1:
-            self.valetspots -= 1
-
-        elif response.lower() == "premium valet" and self.valetspots >= 1:
-            self.valetspots -= 1
-
-    elif self.spots == 0:
-        print(f"There are no more spots available. Sorry!")
-        
-
-    def leaveGarage():
-        clear()
+        cost = {}
         exit_response = input("Which service did you use? (Basic/ Valet/ Premium Valet: ")
+        response = input("What was your ticket number?: ")
+        for ticketnum, dict in variable.item():
+            if ticketnum == response:
+                #reference dictionary
+                if paid == True:
+                    self.LeaveGarage()
+                else:
+                    print("Pay your ticket cost")
+                if valet == True:
+                    cost +=10
         # quantity = f'{self.spots}, {self.valetspots}
         if exit_response.lower() == "basic" and <= 10:
             self.spots += 1
+            print("10 dollas")
          
-        elif exit_response
-        
-        self.valetspots += 1
+        elif exit_response.lower() == "Valet" 
+            self.valetspots += 1
+            print("12 dollas")
+        elif exit_response.lower() == "Valet" 
+            self.valetspots += 1
+            print("15 dollaz baby")
+            
+        else: 
+            print("That's not one of the options. Please Try Again")
+
+         #loop through again   
 
 
-    def payticket():
+
+    def Payticket():
         clear()
+        cost = {}
         if not paid:
             you_pay = (f"Don't forget to pay!")
             print(input("Did you pay? (y/n): "))
+            #Print("Bro... ")
         else:
             print("Thank you, have a nice day")
             print("Please remember to leave in 15 mins")
-    
+Garage.printlanding(
 
-#  for i in dict 
-#     for 
+#payticket admin function
+
+
+
+# - - - - - - - - - - - - - - MO Reference Code - - - - - - - - - - - - - - - - - - - - - - - - 
+# class Main:
+#     def print_landing():
+#     print_landing() = Garage()
+#         print("""
+# Welcome to Chris & Teddie's Parking Garage~
+# Options:
+# [1] Basic - $10
+# [2] Valet - $12
+# [3] Premium Valet (includes CarWash) = $15
+# [4] Quit
+# [5] Admin Functions
+#         """)
+    if option = 5:
+        input("Enter password: ")
+        f"Today you made {self.money}"
+
+
+
+
+#   while True:
+#             choice = input("What would you like to do? ")
+#             if choice == '1':
+#                 if my_cart.items == {}:
+#                     print('Your cart is empty... start shopping')
+#                 else:
+#                     my_cart.show()
+#             elif choice == '2':
+#                 my_cart.add()
+#             elif choice == '3':
+#                 if my_cart.items == {}:
+#                     print('your cart is empty... add something before you remove it')
+#                 else:
+#                     my_cart.remove()
+#             elif choice == '4':
+#                 my_cart.checkout()
+#                 break
+#             elif choice == '5':
+#                 Main.showInstructions()
+#             else:
+#                 print("invalid input... please try again.")
