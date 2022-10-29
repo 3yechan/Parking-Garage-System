618cd24f11ae2a98e8c8950be8a8f2e14c83b0d8
ojisamacharles <115052569+ojisamacharles@users.noreply.github.com>
2022-10-29 11:32:30 -0700

    Add files via upload

--- parking_garage.py
+++ parking_garage.py
@@ -3,8 +3,8 @@
 #     print("Parking Lot is full.") 
 #     break
 
-if valet.spo* full: # *8* valet spots 8/64 dedicated to valet service.
-    print("All parking spaces have been taken, \n> all parking spots have been taken")
+# if valet.spot full: 
+#     print("All parking spaces have been taken, \n> all parking spots have been taken")
 
 from unicodedata import name
 from IPython.display import clear_output
@@ -14,11 +14,13 @@ from IPython.display import clear_output
 # leave garage
 # pay ticket
 
-class Garage():
+
+class Garage(): 
     def __init__(self):
-        self.variable = {}
+        self.dict = {}
         self.spots = 10 
-        self.valetspots = 8 
+        self.valetspots = 8
+        self.money = []
 
     def valet():
             if self.valetspots >= 0:
@@ -79,24 +81,71 @@ class Garage():
         else: 
             print("That's not one of the options. Please Try Again")
 
-    def Payticket():
+    def pay_ticket():
         clear()
-        cost = price + penalty
-        penalty = 0 
-
-        if cost >= 0:
-            you_pay = (f"Don't forget to pay! As a penality, we have added $5 to your charge.")
-            penalty += 5
-            #Print("Bro... ")
-        else:
-            print("Thank you, have a nice day")
-            print("Please remember to leave in 15 mins")
+        balance = self.dict(name[1])
+        input("Please pay" {self.dict(name[1])}:)
+        if input >= balance:
+            balance -= input
+            if balance <= 0:
+                print("Thanks for paying!")
+                print("Thank you for choosing CT Parking")
+                print("Please remember to leave in 15 mins")
+        elif input < balance:
+            balance -= input
+            if balance > 0:
+                f"Your balance remaining is {balance}"
+                input("Please pay remaining balance:")
 
     def Admin(self):
         money = []
         money = cost #access from dict
         money_for_the_day = sum(money)  
-        
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -      
+class Main():
+    def showInstructions():                              
+        print("""
+Welcome to CT Parking
+Options:
+[1] Basic - $10
+[2] Valet - $12
+[3] Premium Valet (includes CarWash) = $15
+[4] Admin Functions
+[5] Quit
+        """)
+
+    def run():
+        Main.showInstructions()
+        my_car = Garage()
+
+        while True:
+            choice = input("What service would you like")
+            if choice == '1':
+                if self.dict == {}:
+                    print("Selected: Basic")
+                    print("Thank you for choosing CT Parking)
+                    self.dict.add(["basic": 10])
+                    pass
+            elif choice == '2':
+                if self.dict == {}:
+                    print("Selected: Valet")
+                    print("Thank you for choosing CT Parking)
+                    self.dict.add(["valet": 12])
+            elif choice == '3':
+                if my_cart.items == {}:
+                    print("Selected: Premium Valet")
+                    print("Thank you for choosing CT Parking)
+                    self.dict.add(["premium valet": 15])
+            elif choice == '4':
+                    input("Enter password: ")
+                    f"Today you made {self.money}"
+            elif choice == '5':
+                    my_car.pay_ticket
+            else:
+                print("invalid input... please try again.")
+
+
+
 
 #-------------------------------------------SCRATCH CODE ------------------------------------------------
 
@@ -144,6 +193,7 @@ class Garage():
         #     #value in dict = price 
 
 # - - - - - - - - - - - - - - MO Reference Code - - - - - - - - - - - - - - - - - - - - - - - - 
+# #control flow
 # class Main:
 #     def print_landing():
 #     print_landing() = Garage()
@@ -156,9 +206,47 @@ class Garage():
 # [4] Quit
 # [5] Admin Functions
 #         """)
-    if option = 5:
-        input("Enter password: ")
-        f"Today you made {self.money}"
+#     if option = 5:
+#         input("Enter password: ")
+#         f"Today you made {self.money}"
+
+# def showInstructions():
+#         print("""
+#         Welcome to ALDI alliance!
+#         Options:
+#         [1] Show Current Cart
+#         [2] Add Item
+#         [3] Remove Item
+#         [4] Quit
+#         [5] Show  Instructions
+#         """)
+
+#     def print_landing(): # this is our valet/basic 
+#     print_landing() = Garage()
+#         print_landing()
+#         while True:
+#             choice = input("What would you like to do? ")
+#             if choice == '1':
+#                 if my_cart.items == {}:
+#                     print('Your cart is empty... start shopping')
+#                 else:
+#                     my_cart.show()
+#             elif choice == '2':
+#                 my_cart.add()
+#             elif choice == '3':
+#                 if my_cart.items == {}:
+#                     print('your cart is empty... add something before you remove it')
+#                 else:
+#                     my_cart.remove()
+#             elif choice == '4':
+#                 my_cart.checkout()
+#                 break
+
+#             elif choice == '5':
+#                 Main.showInstructions()
+
+#             else:
+#                 print("invalid input... please try again.")
 
 
 
